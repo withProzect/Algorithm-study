8bce374c1690cb1b6d398fb103f601f2342e8a03
VDoring <hojun.dev@gmail.com>
2020-06-09 21:43:48 +0900

    Solve Q1110 by VDoring

--- src/VDoring/boj/1110.cpp
+++ src/VDoring/boj/1110.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int input;
-	int units, tens; //1의자리, 10의자리
-	int sum = 0; //1, 10의 자리 합계
-	int sum2 = 0;
+	int num, backup_num; //유저 숫자 저장, 유저 숫자 백업본
+	char ar[100]; //숫자 저장하는 배열
 
-	cin >> input; //26
+	cin >> num;
 
-	units = input % 10; //6
-	input /= 10;
-	tens = input % 10; //2
+	backup_num = num;
+	ar[1] = backup_num % 10; //ar[1]에 유저의 1의 자리 숫자를 저장
+	backup_num /= 10; //한자리 수 줄임
+	ar[0] = backup_num % 10; //ar[0]에 유저의 10의 자리 숫자를 저장
 
-	sum = units + tens; //8
-
-	sum2 = units + tens; //14
-
-	units = sum2 % 10; //4
-
-	sum = sum2 % 10;
-
-	..........
-
-	return 0;
+	for (int i = 0; ; i++) {
+		ar[i + 2] = ar[i + 1] + ar[i]; //1의자리수 + 10의자리 끼리 더한 값을 1의자리수 다음 배열칸에 저장한다.
+		if (ar[i + 2] > 9) { //다음 배열칸에 저장한 값이 10이상이면
+			ar[i + 2] = ar[i + 2] % 10; //한자리수를 줄이고 저장한다.
+		}
+		if (ar[0] == ar[i + 1] && ar[1] == ar[i + 2]) { //만약 유저가 입력한 값과 같을경우
+			cout << i + 1; //계산휫수 출력
+			return 0;
+		}
+	}
 }
